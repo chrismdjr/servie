b20ee853487dcff47a6928d771fa760bdce5541e
chrismdjr <chrismdjr@gmail.com>
2022-11-14 18:00:31 -0800

    First concept version with a functional server and client

---
+++ test-server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from servie import server
+
+
+def custom_start(event_name, server_obj, data, sock):
+    print(f"Started server [{server_obj.host}:{server_obj.port}].")
+
+
+def custom_close(event_name, server_obj, data, sock):
+    print("Closing server...")
+
+
+def custom_disconnect_connection(event_name, server_obj, data, sock):
+    print(f"Client [{data.addr[0]}] has disconnected.")
+
+
+def custom_accept_connection(event_name, server_obj, data, sock):
+    print(f"Client [{data.addr[0]}] has connected.")
+
+
+def custom_service_connection(event_name, server_obj, data, sock):
+    if not data.recv:
+        return
+
+    if data.recv.decode() == "foobar":
+        print(f"Client [{data.addr[0]}] said \"foobar\".")
+        sock.sendall(b"You said \"foobar\".")
+
+    else:
+        print(f"Client [{data.addr[0]}] did not say \"foobar\".")
+        sock.sendall(b"You didn't say \"foobar\".")
+
+
+def main():
+    servie_server = server.Server("0.0.0.0", 4242, server_events={
+        "on_start": custom_start,
+        "on_close": custom_close,
+        "on_disconnect_connection": custom_disconnect_connection,
+        "on_accept_connection": custom_accept_connection,
+        "on_service_connection": custom_service_connection,
+    })
+
+    servie_server.start()
+
+
+if __name__ == "__main__":
+    main()
